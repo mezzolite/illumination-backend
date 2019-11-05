10a78f249f2bf2f65828e36f3f48a871383bcc63
mezzolite <mez.charney@gmail.com>
2019-11-05 13:39:08 -0700

    seeds words table

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,17 +4,20 @@ require 'json'
 Word.destroy_all
 Idea.destroy_all
 
-
-response = RestClient.get("https://wordsapiv1.p.mashape.com/words/light", headers = {
-    "#{Rails.application.credentials.dig(:word_api_key)}": "#{Rails.application.credentials.dig(:word_api_value)}"
+response = RestClient.get("https://wordsapiv1.p.mashape.com/words/light", headers={
+    "#{Rails.application.credentials.dig(:words_api_key)}": Rails.application.credentials.dig(:words_api_value)
     })
     
+    
 all_definitions = JSON.parse(response.body)["results"]
+    
 
-all_definitions.each do |definition| 
-    definition[:synonyms].each do |synonym|
-        Word.create(
-            text: synonym
-        )
+all_definitions.each do |definition|
+    if definition["synonyms"]
+        definition["synonyms"].each do |synonym|
+            Word.create(
+                text: synonym
+            )
+        end
     end
 end
